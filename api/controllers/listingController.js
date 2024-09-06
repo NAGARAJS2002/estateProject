2b0e9c409a12c8c0550df2cd42129286f8ad67d8
NAGARAJS2002 <nr2277644@gmail.com>
2024-09-06 11:10:29 +0530

    add image slider to the listing page

--- api/controllers/listingController.js
+++ api/controllers/listingController.js
@@ -47,16 +47,14 @@ try{
 }
 }
   
-export const getListing = async (req,res,next) =>{
-  try{
+export const getListing = async (req, res, next) => {
+  try {
     const listing = await Listing.findById(req.params.id);
-  if (!listing) {
-     return next(errorHandler(404, 'Listing not found'));
-  } ;
-  res.status(200).json(listing);
+    if (!listing) {
+      return next(errorHandler(404, 'Listing not found!'));
+    }
+    res.status(200).json(listing);
+  } catch (error) {
+    next(error);
   }
-
-   catch(err){
- next(err)
-  }
-}
+};
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,8 @@ import SignIn from "./pages/SignIn"
 import Header from "./components/Header"
 import PrivateRoute from "./components/PrivateRoute"
 import CreateListing from "./pages/CreateListing"
-import UpdateListing from "./pages/UpdateListing"
+import UpdateListing from "./pages/UpdateListing";
+import Listing from "./pages/Listing"
 
 
 
@@ -18,13 +19,15 @@ export default function App() {
           <Routes>
             <Route  path="/" element={<Home/>}/>
             <Route path='/about' element={<About/>}/>
+            <Route path='/sign-up' element={<SignUp/>}/>
+            <Route path='/sign-in' element={<SignIn/>}/>
+            <Route path="/listing/:listingId" element={<Listing/>}/>
             <Route  element={<PrivateRoute/>}>
             <Route path='/profile' element={<Profile/>}/>
             <Route path='/create-listing' element={<CreateListing/>}/>
             <Route path='/update-listing/:listingId' element={<UpdateListing/>}/>
             </Route>
-            <Route path='/sign-up' element={<SignUp/>}/>
-            <Route path='/sign-in' element={<SignIn/>}/>
+           
           </Routes>
           </BrowserRouter>
   )
